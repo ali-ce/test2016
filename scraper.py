3f816524f1f6d105f9d133c14a03fbea88dc445f
Alice <alice@silk.co>
2016-02-08 14:24:48 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,40 +7,32 @@ import urllib
 #Build queries 1: list URLS of awards per year
 year_list = []
 this_edition = 87
-current_edition = 1
+start_edition = 1
 while current_edition <= this_edition:
   year_url = "http://awardsdatabase.oscars.org/ampas_awards/BasicSearch?action=searchLink&displayType=1&BSFromYear="+str(current_edition)
-  current_edition = current_edition+1
+  start_edition = start_edition+1
   year_list.append(year_url)
-#Build queries2: list of URLS per single award per year
-
+  
+#Build queries 2: list of URLS per single award per year
 award_list=[]
 for url in year_list:
   html_year = requests.get(url).text
   root_year = lxml.html.fromstring(html_year)
   category_id = root_year.xpath("//dl/div[1]/a/@href")[0].partition("CategoryExact=")[2].partition("&")[0]
-  category_url = url+"&BSCategoryExact="+category_id
-  print category_url
+  award_url = url+"&BSCategoryExact="+category_id
+  award_url.append(award_list)
+
+#Get info about each nomination for that category & year combination
+for url in award_list:
+  # Go to first nomination and get the nominee(s)name
+  html_nomination = requests.get(url).text
+  root_nomination = lxml.html.fromstring(html_nomination)
+  nominees = root_nomination.xpath("//b/a/text()")[0]
+  print nominees url
   break
+  
 
-
-
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
 # root.cssselect("div[align='left']")
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
