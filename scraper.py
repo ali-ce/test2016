b3f142910cb50a03f73e0fcc285a04ccb29a85d5
Alice <alice@silk.co>
2016-02-08 17:19:45 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -29,7 +29,7 @@ for url in award_list:
   nominations_text = "|".join(text.text_content() for text in root_nomination.xpath("//tr[td]"))
   nominations_id = "|".join(text.partition("NominationID=")[2]+url for text in root_nomination.xpath("//tr/td/div/a/@href"))
   nominations_movie = "|".join(text.text_content().partition(" -- ")[2] for text in root_nomination.xpath("//tr[td]"))
-  nominees_list = []
+  nominations_nominees = ""
   for url in root_nomination.xpath("//tr/td/div/a/@href"):
     nominations_url ="http://awardsdatabase.oscars.org/ampas_awards/BasicSearch?action=searchLink&displayType=6&BSNominationID="+url.partition("NominationID=")[2]  
     nominees_list.append(nominations_url)
@@ -37,8 +37,10 @@ for url in award_list:
     html_nominees = requests.get(url).text
     root_nominees = lxml.html.fromstring(html_nominees)
     nominees = ";".join(name.text_content() for name in root_nominees.xpath("//b/a"))
-    print nominees
-    break
+    nominations_nominees+"|"+nominees
+  print nominations_nominees
+    
+    
   #nominations_nominees = "|".join(text for text in root_nomination.xpath("//tr[td]"))
   
   #root_nomination.xpath("//dl/table")
