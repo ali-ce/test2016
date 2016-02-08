d765ac7b74d68f07091b8d7ef9f3b4b7a353d145
Alice <alice@silk.co>
2016-02-08 17:44:19 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -29,8 +29,12 @@ for url in award_list:
   text = "|".join(text.text_content() for text in root_nomination.xpath("//tr[td]"))
   unique_id = "|".join(text.partition("NominationID=")[2]+url for text in root_nomination.xpath("//tr/td/div/a/@href"))
   movie = "|".join(text.text_content().partition(" -- ")[2] for text in root_nomination.xpath("//tr[td]"))
-  win = "|".join(text for text in root_nomination.xpath("//tr/td[1]/text()"))
-  print win
+  for text in root_nomination.xpath("//tr[td]")):
+    if text[0]="*":
+      win = "Yes"
+    else:
+      win = "No"
+    print win    
   #Iterate in each row to get the clean names of the nominees for each nominations
   nominee_url_list=[]
   for url in root_nomination.xpath("//tr/td/div/a/@href"):
