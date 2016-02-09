f3fe82d5e0ba74f8a102ddd22fa3709259937f6c
Alice <alice@silk.co>
2016-02-09 11:31:57 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,7 +20,10 @@ for url in year_list:
   category_id_list=[]
   for category in root_year.xpath("//div/a/@href"):
     category_id = category.partition("CategoryExact=")[2].partition("&")[0]
-    category_id_list.append(category_id)
+    if str(category_id) == "":
+      break
+    else:
+      category_id_list.append(category_id)
 #      award_url = url+"&BSCategoryExact="+category_id
 #      award_list.append(award_url)
   print category_id_list
